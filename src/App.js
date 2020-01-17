e9695cb3cf7bbd6d07be04a4a5640b23ea3cbeed
Yonatan Melka <ybmelka2@gmail.com>
2020-01-16 21:07:43 -0600

    Add sucessful api fetching

--- src/App.js
+++ src/App.js
@@ -4,11 +4,41 @@ import React, { Component } from 'react';
 import SearchForm from './Components/SearchForm';
 import Navigation from './Components/Navigation';
 import Gallery from './Components/Gallery';
+import axios from 'axios';
+
+// Import Api-key
+import apiKey from './config';
+const numOfPhotos = 24;
+
+export default class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      photos: [],
+      loading: true
+    }
+  }
+
+  componentDidMount() {
+    this.performSearch('cats')
+  }
 
 
-class App extends Component {
+  performSearch = (query) => {
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=${numOfPhotos}&format=json&nojsoncallback=1`)
+      .then((res) => {
+        this.setState({
+          photos: res.data.photos.photo
+        })
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+  }
 
   render() {
+    console.log(this.state.photos);
     return (
       <div className="container">
         <SearchForm />
@@ -19,5 +49,3 @@ class App extends Component {
   }
 
 }
-
-export default App;
