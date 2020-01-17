22d4400fc5384023ef9c2cf98e4c7406cf7f845a
Yonatan Melka <ybmelka2@gmail.com>
2020-01-16 20:20:54 -0600

    Create component files

---
+++ src/Components/Gallery.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Gallery = () => {
+
+  return (
+    <div className="photo-container">
+
+    </div>
+  )
+}
+
+export default Gallery;
---
+++ src/Components/Navigation.js
@@ -0,0 +1,28 @@
+import React from 'react';
+// import { NavLink } from 'react-router-dom';
+
+
+const Navigation = () => {
+  return (
+    <nav className="main-nav">
+      <ul>
+        <li>Cats</li>
+        <li>Dogs</li>
+        <li>Computers</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation
+
+
+/*
+<nav class="main-nav">
+  <ul>
+    <li><NavLink to='#'>Cats</NavLink></li>
+    <li><NavLink to='#'>Dogs</NavLink></li>
+    <li><NavLink to='#'>Computers</NavLink></li>
+  </ul>
+</nav>
+*/
