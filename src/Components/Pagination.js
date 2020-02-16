c62ab8c3a840ed6ba2e7fdcb36e51a8eb297c595
Yonatan Melka <ybmelka@gmail.com>
2020-02-15 21:12:28 -0600

    Add next button handler

--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 
 class Pagination extends Component {
+  state = {
+    pageStart: 1,
+  };
+
+  handleNext = () => {
+    this.setState(prevState => ({ pageStart: prevState.pageStart + 1 }));
+  };
   render() {
     const arr = [1, 2, 3, 4, 5];
+    const { pageStart } = this.state;
+    console.log(pageStart);
     return (
       <div className='pagination'>
         <ul>
@@ -10,15 +19,18 @@ class Pagination extends Component {
             <button className='greyButton prevButton'> Previous </button>
           </li>
           {arr.map((num, i) => {
-            const pageNum = i + 1;
+            const pageNum = i + pageStart;
             return (
-              <li>
+              <li key={i}>
                 <button className='greyButton'>{pageNum}</button>
               </li>
             );
           })}
           <li>
-            <button className='greyButton nextButton'> Next </button>
+            <button className='greyButton nextButton' onClick={this.handleNext}>
+              {' '}
+              Next{' '}
+            </button>
           </li>
         </ul>
       </div>
